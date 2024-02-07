a2bae621f84ad7725b6012c20dee8ed6933b1d21
Vic D <dvv112345@gmail.com>
2024-02-07 14:24:30 -0500

    Parse information regarding open port

--- app/parse_scan.py
+++ app/parse_scan.py
@@ -3,6 +3,7 @@ from sys import argv
 
 
 def loadJSON(filepath):
+    # Load JSON file into a dictionary
     try:
         f = open(filepath, "r")
     except:
@@ -13,20 +14,22 @@ def loadJSON(filepath):
 
 
 def safe_get(obj, field):
+    # Try to get the field from obj without causing the program to crash
     try:
         result = obj.get(field)
-        if result is None:
-            print("Failed scan")
-            # print(field)
     except:
         result = None
     return result
 
 
 def parse_scan(dict):
+    """ Parse a single scan JSON and extract useful informations.
+    The return result should contain the state of the host, transport layer protocols that find open ports, 
+    and information regarding open ports. """
     result = {}
     scan = safe_get(dict, "scan")
     if scan is None:
+        print("Failed scan")
         return None
     if (len(scan.keys())!=1):
         print("Failed scan")
@@ -35,11 +38,31 @@ def parse_scan(dict):
         network_result = safe_get(scan, network)
         state = safe_get(safe_get(network_result, "status"), "state")
         if state is None:
+            print("Failed scan")
             return None
-        result["network"] = {"state", state}
+        result[network] = {"state": state}
         if (state != "up"):
+            print("Failed scan")
             return None
         fields = ["tcp", "udp"]
+        result[network]["transport_protocol"] = []
+        network_keys = network_result.keys()
+        for field in fields:
+            if field in network_keys:
+                current = safe_get(network_result, field)
+                ports = current.keys()
+                if len(ports) > 0:
+                    result[network]["transport_protocol"].append(field)
+                    result[network][field] = {}
+                for port in ports:
+                    port_result = safe_get(current, port)
+                    if safe_get(port_result, "state") == "open":
+                        result[network][field][port] = {}
+                        name = safe_get(port_result, "name")
+                        if name is not None:
+                            result[network][field][port]["name"] = name
+                
+        
         
     return result
 
