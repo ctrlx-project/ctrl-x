705f01e24c8ae96f97a501c7c77e11f593060a54
Michal Urban <michalurban177@gmail.com>
2024-02-07 14:26:00 -0500

    start of seeder.py

---
+++ app/seeder.py
@@ -0,0 +1,28 @@
+from app import create_app
+from models import db, Scans
+from flask_sqlalchemy import SQLAlchemy
+from scan import app
+import json
+
+from utils import pretty_print
+from pathlib import Path
+
+import os
+
+cwd = os.getcwd()
+directory = cwd + "/seed"
+    
+for file in os.listdir(directory):
+    filename = os.fsdecode(file)
+    if filename.endswith(".json"): 
+        # print(os.path.join(directory, filename))
+        f = open(os.path.join(directory, filename))
+        data = json.load(f)
+        ip = Path(os.path.join(directory, filename)).stem
+        print(os.path.join(directory, filename))
+        with app.app_context():
+            db.session.add(Scans(scan_data=data, ip=ip))
+            db.session.commit()
+        continue
+    else:
+        continue
