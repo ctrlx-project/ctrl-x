a55bd003ba0bfd15893f907558856af7e9fe028a
Charles Liu <71086498+Chliu81@users.noreply.github.com>
2024-02-20 12:12:56 -0500

    Flask frontend
    * ADD initial frontend commit
    * ADD added some functionality for homepage and request for scanning

--- app/app.py
+++ app/app.py
@@ -1,8 +1,9 @@
 from dataclasses import dataclass
 from os import environ
 
-from flask import Flask
+from flask import Flask, render_template, url_for, request, redirect
 from models import db
+import json
 
 
 @dataclass
@@ -22,3 +23,17 @@ def create_app() -> Flask:
     db.init_app(app)
 
     return app
+
+app = create_app()
+
+@app.route("/json", methods=["GET"])
+def send_data():
+    fs = open('./seed/10.1.0.1.json','r')
+    json_dump = fs.read()
+    fs.close()
+    json_data = json.loads(json_dump)
+    return json_data
+
+@app.route("/",methods=["GET"])
+def homepage():
+    return render_template("front_page.html")
