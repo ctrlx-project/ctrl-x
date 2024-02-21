8cab12b1c8171bb82d9f07ce611e1e7fe4d68abe
Joe <joe@osiris.cyber.nyu.edu>
2024-02-21 13:17:30 -0500

    add error_resp, success_resp

--- app/utils.py
+++ app/utils.py
@@ -1,2 +1,19 @@
+from string import ascii_letters, digits
+
+
 def pretty_print(data, **kwargs):
     print("\033[92m{}\033[00m".format(data), **kwargs)
+
+
+def error_resp(message):
+    return {
+        'status': 'error',
+        'message': message
+    }
+
+
+def success_resp(message):
+    return {
+        'status': 'success',
+        'message': message
+    }
