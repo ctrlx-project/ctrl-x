af7579ad9b7673cf1490efc3bffcf7cf3946e8e5
Igor8mr <igormr@nyu.edu>
2024-02-26 15:09:28 -0500

    Made more changes to parse_metasploit

--- app/parse_metasploit.py
+++ app/parse_metasploit.py
@@ -8,11 +8,14 @@ class MetasploitManager:
     def __init__(self, password: str, port:int):
         self.parsed_scan = None
         self.options = None
-        self.manager = MsfRpcClient(password, port)
+        self.manager = MsfRpcClient(password, port=port)
 
-    def change_scan(parsed_scan: dict):
+    def load_scan(self, parsed_scan: dict):
         self.parsed_scan = parsed_scan 
         self.options = {}
+    
+    def load_options(self, options: dict):
+        self.options = options
 
     def analyze_targets(self):
         """Find useful modules from Metasploit manager based on CVE in parsed_scan 
@@ -21,28 +24,35 @@ class MetasploitManager:
             if scan["state"] != "up":
                 continue
             for port, info in scan["ports"].items():
-                cve_set = info.get("vulner")
-                if cve_set:
-                    if not self.options.get(ip):
-                        self.options[ip] = {}
-                    if not self.options[ip].get(port):
-                        self.options[port] = {}
+                cve_set = info.get("vulner", [])
                 for cve in cve_set:
-                    self.options[ip][port][cve] = {}
                     self.get_module(ip, port, cve)
 
     def get_module(self, ip: str, port: str, cve: str):
-        module_list = self.manager.modules.search(cve)
+        """Gets a metasploit module given the target IP, port and a
+        vulnerability.
+    
+        Args:
+            ip (str): The IP address of the target machine.
+            port(int): The port of the target machine.
+            cve (str): The code of the CVE to be exploited.
+        """
+        module_list = self.manager.modules.search(cve)            
         for module in module_list:
             if (module and
                 (module_name := module.get("fullname")) and
                 (module.get("type") == "exploit")):
+
+                self.options[ip] = self.options.get(ip, {})
+                self.options[ip][port] = self.options[ip].get(port, {})
+                self.options[ip][port][cve] = self.options[ip][port].get(cve, {})
+
                 exploit = self.manager.modules.use("exploit", module_name)
                 self.options[ip][port][cve] = {"module_name": module_name}
                 missing = exploit.missing_required
                 for option in exploit.options:
                     if option in missing:
-                        self.options[ip][port][cve][option] = fill_option(option, ip, port, my_ip)
+                        self.options[ip][port][cve][option] = self.fill_option(option, ip, port, my_ip)
                     else:
                         self.options[ip][port][cve][option] = exploit[option]
                 self.options[ip][port][cve]['worked'] = None
@@ -57,11 +67,24 @@ class MetasploitManager:
         elif option_upper == "RPORT":
             return target_port
         else:
-            print("Option does not exist")
+            print("Option not configured.")
             return None
     
     def exploit(ip: str, port: int, cve: str):
-        options = self.options[ip][port][cve]
+        """Exploits the target given its IP, port and a vulnerability.
+    
+        Args:
+            ip (str): The IP address of the target machine.
+            port(int): The port of the target machine.
+            cve (str): The code of the CVE to be exploited.
+
+        Returns:
+            (int) Session number.
+        """
+        try:
+            options = self.options[ip][port][cve]
+        except KeyError:
+            return -1
         exploit = self.manager.modules.use("exploit", options["module_name"])
         for option in options:
             module[option] = options[options]
@@ -74,4 +97,6 @@ if __name__ == "__main__":
         exit(1)
     result = parse_from_JSON(argv[1])
     manager = MetasploitManager('JW19ka73', 55552)
-    manager.analyze_targets(result)
+    manager.change_scan(result) 
+    manager.analyze_targets()
+    print(manager.options)
