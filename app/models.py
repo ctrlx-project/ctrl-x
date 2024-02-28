9cf47c77f482f5204b85a36f9ea6ab195f85bf0c
Joe <joe@osiris.cyber.nyu.edu>
2024-02-28 12:59:50 -0500

    remove ScanJob. Functionality integrated into scan

--- app/models.py
+++ app/models.py
@@ -7,28 +7,24 @@ class Scan(db.Model):
     __tablename__ = 'scans'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     ip = db.Column(db.String(15), nullable=False)
+    subnet = db.Column(db.String(18))
     scan_data = db.Column(db.JSON, nullable=False)
     start_time = db.Column(db.DateTime, nullable=False, server_default=db.func.now())
-    end_time = db.Column(db.DateTime, nullable=False, server_default=db.func.now())
-    job = db.relationship('ScanJob', backref='scan', uselist=False)
-
-
-class ScanJob(db.Model):
-    __tablename__ = 'scan_jobs'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    ip = db.Column(db.String(15), nullable=False)
-    start_time = db.Column(db.DateTime(timezone=True), server_default=db.func.now())
-    end_time = db.Column(db.DateTime(timezone=True))
+    end_time = db.Column(db.DateTime, nullable=False)
+    job = db.relationship('ScanJob', backref='scan')
     status = db.Column(db.String(15))
-    scan_id = db.Column(db.Integer, db.ForeignKey('scans.id', ondelete='CASCADE', onupdate='CASCADE'))
+
+
+class Setting(db.Model):
+    __tablename__ = 'settings'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    key = db.Column(db.String(255), nullable=False)
+    value = db.Column(db.Text)
 
     @property
     def info(self):
         return {
             'id': self.id,
-            'ip': self.ip,
-            'start_time': self.start_time,
-            'end_time': self.end_time,
-            'status': self.status,
-            'scan_id': self.scan_id
+            'key': self.key,
+            'value': self.value
         }
