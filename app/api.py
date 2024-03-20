09fafdf938bb42979ce51a803c8a5cba7c217d3d
charle <cl5522@nyu.edu>
2024-03-19 21:59:53 -0400

    ADD/FIX added POST method to getscans api and changed select statements/queries to non-legacy version

--- app/api.py
+++ app/api.py
@@ -11,21 +11,40 @@ def _api():
 
 @api.route('/getscans', methods=['GET'])
 def get_scans():
-    fields = ['id', 'ip', 'scan_data', 'start_time', 'end_time', 'status']
-    num_fields = len(fields)
-    # if GET method, return all scans in Database
+
+    # Function to parse result object and return a list of python dictionaries
+    def parse_query(query, fields) -> list:
+        num_fields = len(fields)
+        ret = []
+        for scan in query:
+            scans = [scan.id, scan.ip, scan.scan_data, scan.start_time, scan.end_time, scan.status]
+            scan_dict = {fields[idx]:scans[idx] for idx in range(num_fields)}
+            ret.append(scan_dict)
+        return ret
+    
+    fields = ('id', 'ip', 'scan_data', 'start_time', 'end_time', 'status')
+    # if GET method, return all scans in database
     if request.method == 'GET':
-        if query := Scan.query.all():
-            ret = []
-            for scan in query:
-                scans = [scan.id, scan.ip, scan.scan_data, scan.start_time, scan.end_time, scan.status]
-                scan_dict = {fields[idx]:scans[idx] for idx in range(num_fields)}
-                ret.append(scan_dict)
-            return ret
+        stmt = db.select(Scan)
+        if db.session.execute(stmt).first:
+            result = db.session.execute(stmt).scalars().all()
+            return parse_query(result, fields)
         else:
             return error_resp("No scans yet!")
-        
     # if POST method, return scans with matching IP's
     elif request.method == "POST":
-        ...
+        if request_ip := str(escape(request.form.ip)):
+            stmt = db.select(Scan).where(Scan.ip==request_ip)
+            if db.session.execute(stmt).first():
+                result = db.session.execute(stmt).scalars().all()
+                return parse_query(result,fields)
+            else:
+                return error_resp(f"Scans with ip {request_ip} not found.")
+        else:
+            return error_resp(f"IP is required for a POST request to this endpoint.")
+
+
+
+
+
 
