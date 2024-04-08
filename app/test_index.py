d3ba006316622aca8a4d3405f9587f10cc0972cc
Michal Urban <michalurban177@gmail.com>
2024-04-08 13:44:12 -0400

    removed old index test code

--- app/test_index.py
+++ app/test_index.py
@@ -6,23 +6,6 @@ from api import api
 
 import os 
 
-"""
-@pytest.fixture
-def client():
-    with api.test_client() as client:
-        yield client
-
-class TestIndext:
-    def test_index(self, client):
-        res = client.get('/')
-        assert res.status_code == 200
-
-def test_index():
-    app_instance = create_app()
-    with app_instance.test_client() as test_client:
-        response = test_client.get('/')
-        assert response.status_code == 200 """
-
 def test_home_page():
     """
     GIVEN a Flask application configured for testing
