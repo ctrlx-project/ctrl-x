017c8f0ef7f8009da7afc57c4137befbe367a9d2
Joe <joe@osiris.cyber.nyu.edu>
2024-04-10 13:09:58 -0400

    add celery

--- app/scannerd.py
+++ app/scannerd.py
@@ -3,10 +3,11 @@ This app runs as a separate process and is responsible for scanning the network.
 from the main app. It shares the codebase with main app.
 
 How to use:
-    /POST: Dispatch a scan job
-        Form Params:
+    Dispatch a scan job from main app:
+        Worker Params:
             - ip: IP/FQDN of the scan job to be dispatched
         This also changes the status of the scan job (status) to 'running' and sets the start time in the database.
+        When it is done, it updates the status to 'finished' and sets the end time.
 """
 
 import nmap
@@ -14,8 +15,6 @@ import concurrent.futures
 import threading
 
 from sys import stderr
-from flask import request
-from utils import success_resp, error_resp
 from datetime import datetime
 from dns import resolver
 from dataclasses import dataclass
@@ -24,8 +23,17 @@ from ipaddress import IPv4Network, AddressValueError
 
 from models import db, Scan, Setting
 from app import create_app
+from utils import success_resp, error_resp
+from celery import shared_task
 
 app = create_app()
+celery_app = app.extensions["celery"]
+celery_app.set_default()
+
+
+@shared_task(ignore_result=False, name='scannerd.test')
+def test_scannerd(message):
+    return "Pong" if message == "Ping" else "Failed"
 
 
 @dataclass
@@ -39,7 +47,8 @@ class Env:
     def update(self):
         with app.app_context():
             self.nmap_scan_args = Setting.query.filter_by(key='nmap_scan_args').first().value or self.nmap_scan_args
-            self.resolver.nameservers = [Setting.query.filter_by(key='nameserver').first().value, '8.8.8.8'] or self.resolver.nameservers
+            self.resolver.nameservers = [Setting.query.filter_by(key='nameserver').first().value,
+                                         '8.8.8.8'] or self.resolver.nameservers
 
     def __update__(self):
         while not self.stop_event.is_set():
@@ -59,20 +68,20 @@ env = Env()
 
 def nm_scan(ip: str):
     nm = nmap.PortScanner()
-    scan = Scan(ip=ip, status='running')
-    db.session.add(scan)
+    scan_ = Scan(ip=ip, status='running')
+    db.session.add(scan_)
     db.session.commit()
 
     try:
         nm.scan(ip, arguments=env.nmap_scan_args)
         scan_data = nm.analyse_nmap_xml_scan()
-        scan.update(dict(scan_data=scan_data, status='complete', end_time=datetime.now()))
+        scan_.update(dict(scan_data=scan_data, status='complete', end_time=datetime.now()))
     except Exception as e:
-        scan.status = 'failed'
+        scan_.status = 'failed'
         print(f"Error scanning {ip}: {e}", file=stderr)
 
     finally:
-        db.session.add(scan)
+        db.session.add(scan_)
         db.session.commit()
 
 
@@ -87,23 +96,23 @@ def scan_job(ip_block: list[str]):
             future.result()
 
 
-@app.route('/', methods=['POST'])
-def index():
+@shared_task(ignore_result=False, bind=True, name='scannerd.scan')
+def scan(raw_input: str):
     # takes in domain name, or ip in the format a.b.c.d/CIDR
-    ip_block = raw_input = request.form.get('ip')
+    ip_block = raw_input
     if ip_block is None:
         return error_resp('IP/FQDN is required')
 
     try:
         ip_block = IPv4Network(ip_block, strict=False)  # will raise AddressValueError if not a valid IP
-    except AddressValueError: # if not a valid IP, try to resolve it
+    except AddressValueError:  # if not a valid IP, try to resolve it
         ip_block = env.resolver.query(ip_block, 'A')[0].to_text()
     except resolver.NXDOMAIN:
         return error_resp('Invalid IP or NXDOMAIN')
 
-    threading.Thread(target=scan_job, args=(str(ip_block),)).start()
+    scan_job(ip_block)
     return success_resp(f"Scan job for {raw_input} dispatched at {datetime.now()}")
 
 
 if __name__ == '__main__':
-    app.run(port=8000, debug=True)
+    pass
