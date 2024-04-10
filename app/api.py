b2a01d0bac28f81bc20328a52c5ad42c5dc47ad3
Joe <joe@osiris.cyber.nyu.edu>
2024-04-10 13:11:52 -0400

    add test route

--- app/api.py
+++ app/api.py
@@ -1,6 +1,9 @@
 from flask import Blueprint
 from models import db, Scan
 from utils import success_resp, error_resp
+from scannerd import scan, test_scannerd
+from celery.result import AsyncResult
+from time import sleep
 
 api = Blueprint('api', __name__, static_folder='static', template_folder='templates')
 
@@ -9,3 +12,16 @@ api = Blueprint('api', __name__, static_folder='static', template_folder='templa
 def _api():
     return success_resp("API is running")
 
+
+@api.route('/test_mq')
+def test_mq():
+    result = test_scannerd.delay("Ping")
+    sleep(1)
+    result = AsyncResult(result.id)
+    return {
+        "id": result.id,
+        "ready": result.ready(),
+        "success": result.successful(),
+        "message": "Ping",
+        "reply": result.result if result.ready() else None
+    }
