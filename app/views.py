92ef5214824ce477a9aecef8116513982f5b26d1
dvv112345 <dvv112345@gmail.com>
2024-04-15 14:56:50 -0400

    Added markdown conversion for table

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, Markup
 import os
 import markdown
 
@@ -13,11 +13,12 @@ def home():
 def show_scans():
     return render_template('general_scans.html')
 
-@index.route('/scans')
-def show_scans():
+@index.route('/reports')
+def show_report():
     cur_dir = os.path.dirname(__file__)
     test_path = os.path.join(cur_dir, "seed/reports/finalReport.md")
     test_file = open(test_path, "r")
     report_markdown = test_file.read()
-    report_html = markdown.markdown(report_markdown)
+    report_html = markdown.markdown(report_markdown, extensions=['tables'])
+    report_html = Markup(report_html)
     return render_template('report.html', report=report_html)
