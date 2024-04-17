277bebcea814a3218fcee54b41750ab4714c5992
Joe <joe@osiris.cyber.nyu.edu>
2024-04-17 16:37:46 -0400

    add scan as task

--- app/scannerd.py
+++ app/scannerd.py
@@ -5,25 +5,21 @@ from the main app. It shares the codebase with main app.
 How to use:
     Dispatch a scan job from main app:
         Worker Params:
-            - ip: IP/FQDN of the scan job to be dispatched
+            - ip_block: IP/FQDN of the scan job to be dispatched
         This also changes the status of the scan job (status) to 'running' and sets the start time in the database.
-        When it is done, it updates the status to 'finished' and sets the end time.
+        When it is done, it updates the status to 'complete' and sets the end time.
 """
 
 import nmap
 import concurrent.futures
-import threading
 
 from sys import stderr
 from datetime import datetime
-from dns import resolver
-from dataclasses import dataclass
-from time import sleep
-from ipaddress import IPv4Network, AddressValueError
+from typing import NoReturn as Never
 
-from models import db, Scan, Setting
-from app import create_app
-from utils import success_resp, error_resp
+from models import db, Scan
+from app import env, create_app
+from utils import resolve_ip_block
 from celery import shared_task
 
 app = create_app()
@@ -38,53 +34,29 @@ def test_scannerd(message):
     return "Pong" if message == "Ping" else "Failed"
 
 
-@dataclass
-class Env:
-    nmap_scan_args = "-Pn -sS -sV -A -T5 --script=default,discovery,vuln"
-    resolver = resolver.Resolver()
-    resolver.nameservers = ['1.1.1.1', '8.8.8.8']
-
-    stop_event = threading.Event()
-
-    def update(self):
-        with app.app_context():
-            self.nmap_scan_args = Setting.query.filter_by(key='nmap_scan_args').first().value or self.nmap_scan_args
-            self.resolver.nameservers = [Setting.query.filter_by(key='nameserver').first().value,
-                                         '8.8.8.8'] or self.resolver.nameservers
-
-    def __update__(self):
-        while not self.stop_event.is_set():
-            try:
-                self.update()
-            except Exception as e:
-                print(f"Error updating settings: {e}", file=stderr)
-            finally:
-                sleep(3)
-
-    def __post_init__(self):
-        threading.Thread(target=self.__update__, daemon=True).start()
-
-
-env = Env()
-
-
 def nm_scan(ip: str):
     nm = nmap.PortScanner()
-    scan_ = Scan(ip=ip, status='running')
-    db.session.add(scan_)
-    db.session.commit()
+    with app.app_context():
+        scan_ = Scan(ip=ip)
+        scan_.status = 'running'
+        db.session.add(scan_)
+        db.session.commit()
 
     try:
         nm.scan(ip, arguments=env.nmap_scan_args)
         scan_data = nm.analyse_nmap_xml_scan()
-        scan_.update(dict(scan_data=scan_data, status='complete', end_time=datetime.now()))
+        with app.app_context():
+            scan_.scan_data = scan_data
+            scan_.status = 'complete'
+            scan_.end_time = datetime.now()
+            db.session.add(scan_)
+            db.session.commit()
     except Exception as e:
-        scan_.status = 'failed'
+        with app.app_context():
+            scan_.status = 'failed'
+            db.session.add(scan_)
+            db.session.commit()
         print(f"Error scanning {ip}: {e}", file=stderr)
-
-    finally:
-        db.session.add(scan_)
-        db.session.commit()
 
 
 def scan_job(ip_block: list[str]):
@@ -98,23 +70,15 @@ def scan_job(ip_block: list[str]):
             future.result()
 
 
-@shared_task(ignore_result=False, bind=True, name='scannerd.scan')
-def scan(raw_input: str):
-    # takes in domain name, or ip in the format a.b.c.d/CIDR
-    ip_block = raw_input
-    if ip_block is None:
-        return error_resp('IP/FQDN is required')
-
-    try:
-        ip_block = IPv4Network(ip_block, strict=False)  # will raise AddressValueError if not a valid IP
-    except AddressValueError:  # if not a valid IP, try to resolve it
-        ip_block = env.resolver.query(ip_block, 'A')[0].to_text()
-    except resolver.NXDOMAIN:
-        return error_resp('Invalid IP or NXDOMAIN')
-
-    scan_job(ip_block)
-    return success_resp(f"Scan job for {raw_input} dispatched at {datetime.now()}")
-
-
-if __name__ == '__main__':
-    pass
+@shared_task(ignore_result=True, name='scannerd_scan', autoretry_for=(Exception,), retry_backoff=True,
+             retry_jitter=True, retry_kwargs={'max_retries': 3})
+def scan(ip_block: str) -> Never:
+    """
+    Takes in ip in the format a.b.c.d/CIDR, a.b.c.d, or domain name; or id of scheduled scan job
+    Args:
+        ip_block:  IP/FQDN of the scan job to be dispatched
+    Returns:
+        dict: Response message
+    """
+    if ip_block:
+        scan_job(resolve_ip_block(ip_block))
