08fdb3ef70dabac94081899e34fcbf9c63764a30
Joe <joe@osiris.cyber.nyu.edu>
2024-04-17 16:37:54 -0400

    add scan endpoint

--- app/api.py
+++ app/api.py
@@ -1,6 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from models import db, Scan
-from utils import success_resp, error_resp
+from utils import success_resp, error_resp, validate_scan_job
 from scannerd import scan, test_scannerd
 from celery.result import AsyncResult
 from time import sleep
@@ -13,7 +13,7 @@ def _api():
     return success_resp("API is running")
 
 
-@api.route('/test_mq')
+@api.route('/test-scan')
 def test_mq():
     result = test_scannerd.delay("Ping")
     sleep(1)
@@ -25,3 +25,15 @@ def test_mq():
         "message": "Ping",
         "reply": result.result if result.ready() else None
     }
+
+
+@api.route('/scan', methods=['POST'])
+def _scan_():
+    ip_block = request.form.get('ip_block')
+    if not ip_block:
+        return error_resp('IP/IP block/FQDN is required')
+    if ip_block := validate_scan_job(ip_block):
+        scan.delay(ip_block=ip_block)
+        return success_resp('Scan job dispatched')
+    else:
+        return error_resp('Invalid IP/FQDN')
