213c48d616c1d5fcadd63579f04a273c238c8ec6
Vic D <dvv112345@gmail.com>
2024-04-21 18:05:03 -0400

    Registration

--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ from flask_login import login_user, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from models import db, User
 from utils import error_resp, success_resp
+import re
 
 index = Blueprint('index', __name__, static_folder='static', template_folder='templates')
 
@@ -73,3 +74,27 @@ def logout():
 def register():
     if request.method == "GET":
         return render_template("register.html")
+    elif request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        passwordSecond = request.form.get("password2")
+        if not (username and password and passwordSecond):
+            return render_template("register.html", registerError = "Please complete all the fields")
+        if password != passwordSecond:
+            return render_template("register.html", registerError = "Passwords do not match")
+        if len(password) < 8 or not re.search("[a-zA-Z]", password) or not re.search("[0-9]", password):
+            registerError = "<div class='text-start'>Password do not meet the requirements:\
+            \n<ul><li>Password needs to have at least 8 characters</li>\
+            \n<li>Password needs to contain at least one letter</li>\
+            \n<li>Password needs to contain at least one number</li></ul></div>";
+            return render_template("register.html", registerError = registerError)
+        user = User.query.filter_by(username=username).first()
+        if user:
+            return render_template("register.html", registerError = "This username has already been taken, please try a different username")
+        newUser = User(
+            username=username,
+            password=generate_password_hash(password, "pbkdf2:sha256")
+            )
+        # with app.app_context():
+        db.session.add(newUser)
+        db.session.commit()
