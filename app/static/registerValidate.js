9a700df985694df9cc6b01e9b5da7f7e7a94e7e8
Vic D <dvv112345@gmail.com>
2024-04-21 17:42:29 -0400

    Adds check for register

--- app/static/registerValidate.js
+++ app/static/registerValidate.js
@@ -1,3 +1,31 @@
+function createCheckFunc(form)
+{
+    // Create a function that checks if a form's inputs satasify its requirements
+    return event=>{
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+    };
+}
+
+// Disable submission if there is invalid field
+window.addEventListener(
+'load', 
+function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    let forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission if requirement is not met.
+    for (let form of forms)
+    {
+        form.addEventListener('submit', 
+            createCheckFunc(form)
+        )
+    }
+}
+);
+
 function compPassword()
 {
     // alert("Compare");
