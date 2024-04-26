6d711a329be959bdd1b7c126fd0335be4bd9cd0e
Michal Urban <michalurban177@gmail.com>
2024-04-25 23:25:52 -0400

    added more tests for the api and exploits

--- app/test_index.py
+++ app/test_index.py
@@ -1,4 +1,5 @@
 from flask import request
+from werkzeug.datastructures import Headers
 import requests
 import pytest
 from main import app
@@ -6,25 +7,27 @@ from api import api
 import json
 
 import os 
+os.environ['CONFIG_TYPE'] = 'config.TestingConfig'
 
 def test_home_page():
-    """
-    GIVEN a Flask application configured for testing
-    WHEN the '/' page is requested (GET)
-    THEN check that the response is valid
-    """
-    # Set the Testing configuration prior to creating the Flask application
-    os.environ['CONFIG_TYPE'] = 'config.TestingConfig'
-    #flask_app.run()
-
-    # Create a test client using the Flask application configured for testing
     with app.test_client() as test_client:
         response = test_client.get('/')
         assert response.status_code == 200
 
 def test_api_test_mq():
-    os.environ['CONFIG_TYPE'] = 'config.TestingConfig'
     with app.test_client() as test_client:
         response = test_client.get('api/test-scan')
         res_body = json.loads(response.data.decode('utf-8'))
-        assert res_body["message"] == "Ping"
+        assert response.status_code == 200 and res_body["message"] == "Ping"
+
+def test_api_scan():
+    with app.test_client() as test_client:
+        response = test_client.get('api/scan')
+        res_body = json.loads(response.data.decode('utf-8'))
+        assert response.status_code == 200 and res_body != {}
+
+def test_api_scan_new():
+    with app.test_client() as test_client:
+        response = test_client.post('api/scan-new', data = {'ip_block': '0.0.0.0'})
+        res_body = json.loads(response.data.decode('utf-8'))
+        assert response.status_code == 200 and res_body["status"] != "error"
