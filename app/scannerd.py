29d6b6a66d4ac9e1b692829cd0a577e43dbbd37a
Igor8mr <igormr@nyu.edu>
2024-04-27 20:58:59 -0400

    Updated exploit and scannerd to run on commandline

--- app/scannerd.py
+++ app/scannerd.py
@@ -21,21 +21,15 @@ from models import db, Scan
 from app import env, create_app
 from utils import resolve_ip_block
 from celery import shared_task
-
-app = create_app()
-celery_app = app.extensions["celery"]
-celery_app.set_default()
-
-env.app = app
-
+from sys import argv
+from pprint import pprint
+from json import dump
 
 @shared_task(ignore_result=False, name='scannerd_test')
 def test_scannerd(message):
     return "Pong" if message == "Ping" else "Failed"
 
-
 def nm_scan(ip: str):
-    nm = nmap.PortScanner()
     with app.app_context():
         scan_ = Scan(ip=ip)
         scan_.status = 'running'
@@ -43,8 +37,7 @@ def nm_scan(ip: str):
         db.session.commit()
 
     try:
-        nm.scan(ip, arguments=env.nmap_scan_args)
-        scan_data = nm.analyse_nmap_xml_scan()
+        scan_data = simple_scan(ip)
         with app.app_context():
             scan_.scan_data = scan_data
             scan_.status = 'complete'
@@ -82,3 +75,37 @@ def scan(ip_block: str) -> Never:
     """
     if ip_block:
         scan_job(resolve_ip_block(ip_block))
+
+def simple_scan(ip) -> Never:
+    """
+    Takes in ip in the format a.b.c.d/CIDR, a.b.c.d, or domain name; or id of scheduled scan job
+    Args:
+        ip:  IP of the scan job to be dispatched
+    Returns:
+        dict: Scan data
+    """
+    nm = nmap.PortScanner()
+    nm.scan(ip, arguments=env.nmap_scan_args)
+    return nm.analyse_nmap_xml_scan()
+
+def main():
+    print(f"Scanning IP {argv[1]}")
+    scan_data = simple_scan(argv[1])
+    print(f"Scan results:")
+    pprint(scan_data)
+    if len(argv) > 2:
+        print(f"Saving Scan on {argv[2]}")
+        with open(argv[2], 'w') as fp:
+            dump(scan_data, fp)
+
+if __name__ == "__main__":
+    if len(argv) < 2:
+        print("Usage: py scannerd.py [target_ip] [output_file]")
+        print("\te.g. py scannerd.py 10.10.0.13  seed/nmap/10.10.0.13.json")
+        exit(1)
+    main()
+else:
+    app = create_app()
+    celery_app = app.extensions["celery"]
+    celery_app.set_default()
+    env.app = app
